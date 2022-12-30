5205461dbc9ab916e4983ba7f9104bf1b60e3a93
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2022-12-30 13:39:19 +0200

    Create heroicInventory.js

---
+++ 03.Objects and Composition/heroicInventory.js
@@ -0,0 +1,19 @@
+function heroicInventory(input){
+
+    let obj = {};
+    let jsonArr =[];
+    let jsonFormat = "";
+    for(let element of input){
+        [name, level, items] = element.split(' / ');
+        level=Number(level);
+        items = items ? items.split(','):[];
+        jsonArr.push({name, level, items});
+    }
+    console.log(JSON.stringify(jsonArr));
+//[{"name":"Isacc","level":25,"items":["Apple","GravityGun"]},{"name":"Derek","level":12,"items":["BarrelVest","DestructionSword"]},{"name":"Hes","level":1,"items":["Desolator","Sentinel","Antara"]}]
+
+}
+heroicInventory(['Isacc / 25 / Apple, GravityGun',
+'Derek / 12 / BarrelVest, DestructionSword',
+'Hes / 1 / Desolator, Sentinel, Antara']
+)
